7b686e5af2a950594ffacc9f07857bb31a4a4e30
xKaaZz <93985490+xKaaZz@users.noreply.github.com>
2025-06-20 21:27:05 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,15 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    telegram_id = Column(String, nullable=True)
+    telegram_token = Column(String, nullable=True)
+    
 class TrackCollection(Base):
     __tablename__ = "collections"
 
