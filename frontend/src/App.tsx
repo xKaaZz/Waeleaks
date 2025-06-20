409b98d895128d0e3186caaf9d01fc6c5f07f7d4
xKaaZz <93985490+xKaaZz@users.noreply.github.com>
2025-06-20 21:43:53 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,7 @@
-import { ChakraProvider, Box } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import CollectionList from './components/CollectionList'
-import CollectionDetail from './components/CollectionDetail'
-import AddCollectionForm from './components/AddCollectionForm'
-import AddTrackForm from './components/AddTrackForm'
-import LoginPage from './components/LoginPage'
-import RegisterPage from './components/RegisterPage'
-import UpdateTelegramPage from './components/UpdateTelegramPage'
-
 function App() {
   return (
-    <ChakraProvider>
-      <Router>
-        <Box minH="100vh" bg="gray.50">
-          <Navbar />
-          <Box p={4}>
-            <Routes>
-              <Route path="/" element={<CollectionList />} />
-              <Route path="/collection/:id" element={<CollectionDetail />} />
-              <Route path="/collection/:id/add" element={<AddTrackForm />} />
-              <Route path="/add" element={<AddCollectionForm />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/update-telegram" element={<UpdateTelegramPage />} />
-            </Routes>
-          </Box>
-        </Box>
-      </Router>
-    </ChakraProvider>
+    <div style={{ padding: "2rem" }}>
+      <h1 style={{ color: "red" }}>WAELEAKS FRONT OK</h1>
+    </div>
   )
 }
-
-export default App
