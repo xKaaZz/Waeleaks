18617fc042b8888e81bc8c7a43339d7e4fb99970
xKaaZz <93985490+xKaaZz@users.noreply.github.com>
2025-06-20 22:06:34 +0200

    feef

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@chakra-ui/react"],
+  },
+  server: {
+    port: 5175,
+    host: true,
+    strictPort: true,
+  },
 })
